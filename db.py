ee818752e4462cacf3e62496210c3ae5c6649b00
kevintybussek <kevin08tybussek@web.de>
2026-05-18 08:18:57 +0200

    db.py erstellt

---
+++ db.py
@@ -0,0 +1,29 @@
+import sqlite3
+
+def verbindung_herstellen():
+    return sqlite3.connect("Läufer.db")
+
+def produkte_hinzufügen(name, Vorname, klasse, sportlehrer,zeit):
+    verbindung = verbindung_herstellen()
+    cursor = verbindung.cursor()
+    cursor.execute("""
+    INSERT INTO einkaufsliste (name, vorname, klasse, sportlehrer, zeit)
+    VALUES (?, ?, ?, ?)
+    """, (name, Vorname, klasse, sportlehrer, zeit))
+    verbindung.commit()
+    verbindung.close()
+
+
+
+verbindung = verbindung_herstellen()
+cursor = verbindung.cursor()
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS einkaufsliste (
+    schüler_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    vorname TEXT,
+    klasse TEXT,
+    sportlehrer TEXT,
+    zeit FLOAT
+               )
+               """)
