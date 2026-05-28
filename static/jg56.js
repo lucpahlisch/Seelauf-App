ceb73a8f97135e764103c3ef978d9734d776f90e
Yannes <yannesahsbahs@gmail.com>
2026-05-28 11:56:24 +0200

    JOa so mäßig

---
+++ static/jg56.js
@@ -0,0 +1,14 @@
+const jg56 = {
+    "5a": [
+        { name: "Max Müller", zeit: "12:45" },
+        { name: "Anna Schmidt", zeit: "13:10" }
+    ],
+
+    "5b": [
+        { name: "Tom Becker", zeit: "11:58" }
+    ],
+
+    "5c": [
+
+    ]
+};
