12d8059edb6a61c695d6a2ecb70b6401e7b0bce0
Yannes <yannesahsbahs@gmail.com>
2026-06-04 11:44:30 +0200

    ...

---
+++ static/obs.js
@@ -0,0 +1,22 @@
+const obs = {
+    "E(W)": [
+        { name: "", zeit: "" },
+        { name: "", zeit: "" }
+    ],
+
+    "E(G)": [
+        { name: "", zeit: "" }
+    ],
+
+    "E(S)": [
+        { name: "", zeit: "" }
+
+    ],
+
+    "E(G)": [
+        { name: "", zeit: "" }
+
+    ],
+
+
+};
