4aa8c9959bae4a0605a830f1a93987eb37d99109
Yannes <yannesahsbahs@gmail.com>
2026-06-08 09:00:49 +0200

    gig

--- app.py
+++ app.py
@@ -11,48 +11,78 @@ def startseite():
 @app.route("/verwalten", methods=["GET", "POST"])
 def verwalten():
 
+    conn = sqlite3.connect("Läufer-Schüler.db")
+    conn.row_factory = sqlite3.Row
+    cur = conn.cursor()
+
     if request.method == "POST":
 
-        name = request.form["name"]
-        vorname = request.form["vorname"]
-        jahrgang = request.form["jahrgang"]
-        klasse = request.form["klasse"]
-        zeit = request.form["zeit"]
+        if "hinzufuegen" in request.form:
+            name = request.form["name"]
+            vorname = request.form["vorname"]
+            jahrgang = request.form["jahrgang"]
+            klasse = request.form["klasse"]
+            zeit = request.form["zeit"]
 
-        conn = sqlite3.connect("Läufer-Schüler.db")
-        cur = conn.cursor()
+            cur.execute("""
+                INSERT INTO Laeufer
+                (name, vorname, klasse, jahrgang, zeit)
+                VALUES (?, ?, ?, ?, ?)
+            """, (name, vorname, klasse, jahrgang, zeit))
 
-        cur.execute("""
-            INSERT INTO Laeufer
-            (name, vorname, klasse, jahrgang, zeit)
-            VALUES (?, ?, ?, ?, ?)
-        """, (name, vorname, klasse, jahrgang, zeit))
-
+        if "loeschen" in request.form:
+            schueler_id = request.form["schueler_id"]
+            cur.execute("""
+                DELETE FROM Laeufer
+                WHERE rowid = ?
+            """, (schueler_id,))
         conn.commit()
         conn.close()
 
         return redirect("/verwalten")
 
-    return render_template("verwalten.html")
+    schueler = cur.execute("""
+        SELECT rowid, * FROM Laeufer
+        ORDER BY jahrgang, klasse, name
+    """).fetchall()
+    conn.close()
+
+    return render_template("verwalten.html", schueler=schueler)
 
 @app.route("/zeiten")
 def zeiten():
-
     conn = sqlite3.connect("Läufer-Schüler.db")
     conn.row_factory = sqlite3.Row
-
     cur = conn.cursor()
 
-    daten = cur.execute(
-        "SELECT * FROM Laeufer"
-    ).fetchall()
+    daten = cur.execute("""
+        SELECT * FROM Laeufer
+        ORDER BY jahrgang, klasse, name
+    """).fetchall()
 
     conn.close()
 
-    return render_template(
-        "zeiten.html",
-        daten=daten
-    )
+    return render_template("zeiten.html", daten=daten)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port=8080, debug=True)
+
+def datenbank_pruefen():
+    conn = sqlite3.connect("Läufer-Schüler.db")
+    cur = conn.cursor()
+
+    try:
+        cur.execute("ALTER TABLE Laeufer ADD COLUMN jahrgang TEXT")
+    except sqlite3.OperationalError:
+        pass
+
+    try:
+        cur.execute("ALTER TABLE Laeufer ADD COLUMN zeit TEXT")
+    except sqlite3.OperationalError:
+        pass
+
+    conn.commit()
+    conn.close()
+
+
+datenbank_pruefen()
